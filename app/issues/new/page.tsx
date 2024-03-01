f768d56ef1d05e5246ba20561e7ad0126e9ebe54
nurulermeliasafitri95 <nurulermelia@gmail.com>
2024-03-01 11:26:45 +0800

    Adding a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -17,6 +17,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -31,6 +32,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -43,9 +45,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An unexpected error occured.");
           }
         })}
@@ -64,7 +68,9 @@ const NewIssuePage = () => {
         {errors.description && (
           <ErrorMessage>{errors.description.message}</ErrorMessage>
         )}
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,8 @@ import { z } from "zod";
 
 export const createIssueSchema = z.object({
     title: z.string().min(1, 'Title is requred.').max(255),
-    description: z.string().min(1, 'Description is required.')
+    // description: z.string().min(1, 'Description is required.')
+    description: z
+    .string({ required_error: 'Description is required.' })
+    .min(1, 'Description is required.'),
 });
