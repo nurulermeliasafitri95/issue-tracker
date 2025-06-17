9d5d788e0b6ebe3e31feaf8aee6db2f8d974f8e9
Elia <elia@viprewards.com.my>
2025-06-17 13:22:15 +0800

    Building the Pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello World elia</div>;
+  return (
+    <Pagination itemCount={100} pageSize={10} currentPage={10} />
+  )
 }
