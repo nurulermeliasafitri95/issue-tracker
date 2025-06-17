7768db6f70e88ba5ef9094fff360a5c12f275fa4
Elia <elia@viprewards.com.my>
2025-06-17 15:41:46 +0800

    add Metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,12 @@
-import prisma from "@/prisma/client";
-import { Box, Flex, Grid } from "@radix-ui/themes";
-import { notFound } from "next/navigation";
-import EditIssueButton from "./EditIssueButton";
-import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
-import { getServerSession } from "next-auth";
-import authOptions from "@/app/auth/authOptions";
-import AssigneeSelect from "./AssigneeSelect";
+import prisma from '@/prisma/client';
+import { Box, Flex, Grid } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
+import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
+import AssigneeSelect from './AssigneeSelect';
 
 interface Props {
   params: { id: string };
@@ -22,7 +22,7 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+    <Grid columns={{ initial: '1', sm: '5' }} gap="5">
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
@@ -38,5 +38,14 @@ const IssueDetailPage = async ({ params }: Props) => {
     </Grid>
   );
 };
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) }});
+
+  return {
+    title: issue?.title,
+    description: 'Details of issue ' + issue?.id
+  }
+}
 
 export default IssueDetailPage;
